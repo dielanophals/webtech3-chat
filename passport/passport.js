9f8bd778c375f955cb0d8888b8be4158762f247f
AqsaIntizar <aqsatje@live.be>
2019-05-08 23:36:19 +0200

    signup in orde

---
+++ passport/passport.js
@@ -0,0 +1,7 @@
+const passport = require('passport');
+const User = require('../models/User');
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());

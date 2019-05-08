7e7de2b89f439c8724781a5988bb16fba263fa7a
AqsaIntizar <aqsatje@live.be>
2019-05-08 20:13:03 +0200

    added controllers folder for api 1st version

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const authController = require("../controllers/auth");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,6 +12,6 @@ router.get('/', function(req, res, next) {
 //wachtwoord uit request halen
 //bycrypt
 //databank
-router.post('/signup', authController.signup);
-router.post('/login',authController.login)
+//router.post('/signup', authController.signup);
+//router.post('/login',authController.login)
 module.exports = router;

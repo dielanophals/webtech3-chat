e62d096f587d042c48afa0b8e93d4fb79eeba7c0
AqsaIntizar <aqsatje@live.be>
2019-05-08 19:58:30 +0200

    require de message route die we hebben aangemaakt

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
 var indexRouter = require('./routes/index');
+const apiMessageRouter = require('./routes/api/v1/messages');
 
 
 

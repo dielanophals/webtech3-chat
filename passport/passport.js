91e92baa45721d850d3eddb2d1c37359a23a6b0a
AqsaIntizar <aqsatje@live.be>
2019-05-09 12:44:43 +0200

    cors added

--- passport/passport.js
+++ passport/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const User = require('../models/User');
 // CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+//heeft vooral te maken met beveiligen van routes
+//omdat we met APIs werken gaan we niet controlleren of we in een bepaalde sessie zitten.
 passport.use(User.createStrategy());
 
+//voor sessions
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());

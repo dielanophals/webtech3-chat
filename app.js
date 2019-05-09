b817343f71a1cec32dd871e8cad2f5903971971e
AqsaIntizar <aqsatje@live.be>
2019-05-09 12:43:11 +0200

    cors added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiMessageRouter = require("./routes/api/v1/messages");
-
+const cors = require('cors');
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/messages', {useNewUrlParser: true});
@@ -17,6 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

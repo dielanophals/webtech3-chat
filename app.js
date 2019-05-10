afbc1718cad9878109c075512e3fb4be0304e527
AqsaIntizar <aqsatje@live.be>
2019-05-10 10:27:53 +0200

    bug fixed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
 require('dotenv').config();
-mongoose.connect(process.env.MONGODB, {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/messages', {useNewUrlParser: true});
 
 var app = express();
 

402b51c9e6c6cd0a172f6ce53fb4b764797e8bca
Dielan <dielano@hotmail.be>
2019-05-13 17:12:17 +0200

    redirect to correct api

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/api/v1/todos', {
+fetch('http://localhost:3000/api/v1/messages', {
     headers: {
       'Authorization': 'Bearer ' + localStorage.getItem('token')
     },

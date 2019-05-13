9d49e7a7f2cec580f66a1a2587392208cf2b35ce
Dielan <dielano@hotmail.be>
2019-05-14 01:24:23 +0200

    Update users.js

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -1,6 +1,6 @@
 const User = require('../../../models/User');
 
-const getUsers =  (req, res)=>{
+const getUsers = (req, res) => {
     User.find((err, doc) => {
         if(!err){
             res.json({

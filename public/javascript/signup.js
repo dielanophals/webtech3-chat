f868602cf70035eedb55e9a57b13831c155fca05
Dielan <dielano@hotmail.be>
2019-05-13 14:35:03 +0200

    display whether email or password is filled

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,7 +1,17 @@
 var btnSignup = document.querySelector(".signup button").addEventListener("click", function(e) {
     let username = document.querySelector('#email').value;
-     let password = document.querySelector('#password').value;
-    fetch("http://localhost:3000/users/signup", {
+    let password = document.querySelector('#password').value;
+
+    if(username === ""){
+        let feedback = document.querySelector(".alert");
+        feedback.textContent = "Enter an email!";
+        feedback.classList.remove('hidden');
+    }else if(password === ""){
+        let feedback = document.querySelector(".alert");
+        feedback.textContent = "Enter a password!";
+        feedback.classList.remove('hidden');
+    }else{
+        fetch("http://localhost:3000/users/signup", {
         method: "post",
         headers: {
             'Content-Type': 'application/json'
@@ -16,8 +26,10 @@ var btnSignup = document.querySelector(".signup button").addEventListener("click
         if(json.status == "succes"){
             let feedback = document.querySelector(".alert");
             feedback.textContent = "signup complete!";
-            feedback.classList.remove = "hidden";
-    
+            feedback.classList.remove('hidden');
         }
-    });
+    }).catch(err => {
+        console.log(err)
+    })
+    }
 });

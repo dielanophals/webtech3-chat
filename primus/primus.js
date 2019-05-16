6377a73e69c8ad6fef51ab092ab3a42862c74e43
AqsaIntizar <aqsatje@live.be>
2019-05-16 17:53:28 +0200

    enjoy ;)

--- primus/primus.js
+++ primus/primus.js
@@ -2,6 +2,9 @@ const Primus = require('primus');
 
 let go = (server) => {
    let primus = new Primus(server, {/* options */})
+   primus.on('connection', (spark)=>{
+       console.log('received spark');
+   })
 }
 
 module.exports.go = go;
